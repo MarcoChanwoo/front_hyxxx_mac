69ab1f427e47e7fe32fb42b53f3fc1fe22ef70de
Marco <cwhong8087@gmail.com>
2023-09-10 23:10:15 +0900

    7th commit

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
+import { useNavigate } from '../../../node_modules/react-router-dom/dist/index';
 
 const StyledButton = styled.button`
     border: none;
@@ -35,6 +36,17 @@ const StyledButton = styled.button`
         `}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
+const Button = ({ to, ...rest }) => {
+    const navigate = useNavigate();
+    const onClick = (e) => {
+        if (to) {
+            navigate(to);
+        }
+        if (rest.onclick) {
+            rest.onClick(e);
+        }
+    };
+    return <StyledButton {...rest} onClick={onClick} />;
+};
 
 export default Button;
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 // 포스트 목록
 const PostListPage = () => {
     return (
-        <div>
-            <Button>버튼</Button>
-        </div>
+        <>
+            <Header />
+            <div>안녕하세요</div>
+        </>
     );
 };
 
