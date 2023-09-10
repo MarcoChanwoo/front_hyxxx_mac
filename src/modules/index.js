cd15c771d7846d82aa437cf8281fcf159ede9877
Marco <cwhong8087@gmail.com>
2023-09-10 17:37:08 +0900

    5th commit

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,14 +3,16 @@ import { combineReducers } from 'redux'; // 책에서는 그냥 redux로 나옴
 import auth, { authSaga } from './auth';
 import loading from './loading';
 import { all } from 'redux-saga/effects';
+import user, { userSaga } from './user';
 
 const rootReducer = combineReducers({
     auth,
     loading,
+    user,
 });
 
 export function* rootSaga() {
-    yield all([authSaga()]);
+    yield all([authSaga(), userSaga()]);
 }
 
 export default rootReducer;
